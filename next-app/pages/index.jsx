049bbf7101767f237b765487ac64a616b6f6bb50
Virendra Kumar Patel <virendra.patel@dowjones.com>
2021-10-26 07:08:37 +0530

    Added pages

---
+++ next-app/pages/index.jsx
@@ -0,0 +1,12 @@
+//localhost:3000
+
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="container text-center mt-3">
+      <h1>This Is Homepage</h1>
+      <hr />
+    </div>
+  );
+}
---
+++ next-app/pages/news.jsx
@@ -0,0 +1,11 @@
+// localhost:3000/news
+import React from "react";
+
+export default function NewsPage() {
+  return (
+    <div className="container text-center mt-3">
+      <h1>This Is News Page</h1>
+      <hr />
+    </div>
+  );
+}
